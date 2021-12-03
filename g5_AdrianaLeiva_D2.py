5f621a062ec39cd98ebaf219d52eafe12b278f67
Adrianaleiva <adrileiva.33@gmail.com>
2021-12-03 17:18:36 -0300

    Primera version

---
+++ g5_AdrianaLeiva_D2.py
@@ -0,0 +1,13 @@
+def operaciones(lista):
+    suma=0
+    for num in lista:
+        suma+=num
+    print("Maximo: "+ str(max(lista)))
+    print("Minimo: "+ str(min(lista)))
+    print("Suma: " + str(suma))
+    print("Promedio: "+str(suma/len(lista)))
+    
+    
+                
+
+
